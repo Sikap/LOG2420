dbd60344788c6e8045f972cf7c8bc9da99f0dc3c
Sikap <you@example.com>
2020-03-04 15:24:42 -0500

    Deuxiem Tableaux

--- TP1/Scripts/main.js
+++ TP1/Scripts/main.js
@@ -1,7 +1,11 @@
 let displayFederals = () => {
     console.log('federals');
+    var x = document.getElementById('PartisProvinciaux').style.display="none";
+    var x = document.getElementById('PartisFederaux').style.display="block";
 };
 
 let displayProvincials = () => {
     console.log('provincials');
+    var x = document.getElementById('PartisFederaux').style.display="none";
+    var x = document.getElementById('PartisProvinciaux').style.display="block";
 };
